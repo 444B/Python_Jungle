2a61cc9651cb5d373d718e5cdb2def3c1c061ff7
444B <444B+github@protonmail.ch>
2022-06-05 12:20:36 +0300

    Works

--- salary_calc.py
+++ salary_calc.py
@@ -6,26 +6,28 @@
 
 # ------------------------------------------
 print("\n")
-# gross_yearly_salary = int(input("What is your yearly gross salary?"))
-# yearly_holidays = months = int(input("How many holidays do you get a year?: "))
+gross_yearly_salary = int(input("What is your yearly gross salary?"))
+yearly_holidays = months = int(input("How many holidays do you get a year?: "))
 
 print("\n")
 
-####################################################
+###################################################
 # Working Days Section
-# print("Working Days Section")
-# # working_days = (365 - (58*2) - int(yearly_holidays))
-# print("You work " + str(working_days) + " days a year")
-# print("You have about " + str(round(yearly_holidays/12, 2) ) + " holidays every month")
-# print("For every " + str(working_days/yearly_holidays) + " days worked, you earn a day off")
-# print("This is earned every " +str((working_days/yearly_holidays)/5) + " weeks")
-# print("You should take a 5 day holiday every " + str(round(yearly_holidays/12, 2)) + " weeks")
-# print("\n")
+print("Working Days Section")
+working_days = (365 - (58*2) - int(yearly_holidays))
+print("You work " + str(working_days) + " days a year")
+print("You have about " + str(round(yearly_holidays/12, 2) ) + " holidays every month")
+print("For every " + str(working_days/yearly_holidays) + " days worked, you earn a day off")
+print("This is earned every " +str((working_days/yearly_holidays)/5) + " weeks")
+#This next line is clearly wrong! Need to work out the logic for this in the new version
+print("You should take a 5 day holiday every " + str(round(yearly_holidays/12, 2)) + " weeks")
+print("\n")
 
 ####################################################
 # Salary Section
 # I should absolutely write a function to work this out
 # But I want to work on the logic first and use this as a trial
+# Currently does not use full logic for tax bracket deductions
 print("Salary Section")
 # https://www.cpa-dray.com/en/blog/income-tax-rates-in-israel/
 if gross_yearly_salary <=  77400:
@@ -66,13 +68,11 @@ elif gross_yearly_salary > 663241:
 else:
     print("Incorrect value")
 
-# print(" You earn " + str(gross_yearly_salary) + " before tax")
-# print(" You earn " + str(net_yearly_salary) + " after tax")
-# print(" You make " + str(net_yearly_salary / working_days) + " per day")
-# print(" You make " + str(net_yearly_salary / working_days / 8.5) + " per hour")
-# print(" You make " + str(net_yearly_salary / working_days / 8.5 / 60) + " per minute")
-# print(" You make " + str(net_yearly_salary / working_days / 8.5 / 60 / 60) + " per second")
+print(" You earn " + str(gross_yearly_salary) + " before tax")
+print(" You earn " + str(net_yearly_salary) + " after tax")
+print(" You make " + str(net_yearly_salary / working_days) + " per day")
+print(" You make " + str(net_yearly_salary / working_days / 8.5) + " per hour")
+print(" You make " + str(net_yearly_salary / working_days / 8.5 / 60) + " per minute")
+print(" You make " + str(net_yearly_salary / working_days / 8.5 / 60 / 60) + " per second")
 
 
-####################################################
-# Optimal Holidays Section
